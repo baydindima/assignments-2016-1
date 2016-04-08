a99f4791b8afecc758f1e846589da2350f979fa9
baydindima <QCtEes=9,29)vzW>
2016-04-09 00:12:55 +0300

    First and Second

--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,15 +1,38 @@
 package ru.spbau.mit;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
-    private SecondPartTasks() {}
+    private SecondPartTasks() {
+    }
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths.parallelStream()
+                .flatMap(path -> {
+                            try {
+                                return Files.readAllLines(
+                                        Paths.get(path),
+                                        Charset.defaultCharset()
+                                ).parallelStream();
+                            } catch (IOException e) {
+                                throw new UncheckedIOException(e);
+                            }
+                        }
+                )
+                .filter(s -> s.contains(sequence))
+                .collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
@@ -17,18 +40,42 @@ public final class SecondPartTasks {
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать,
     // какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        Random randomGenerator = new Random();
+        final int experimentCount = 10000;
+        final double radius = 0.5;
+        final double circleCenter = 0.5;
+        final double squadPow = 2;
+        long count = Stream.generate(() ->
+                        Math.sqrt(Math.pow(randomGenerator.nextDouble() - circleCenter, squadPow)
+                                + Math.pow(randomGenerator.nextDouble() - circleCenter, squadPow)) <= radius
+        ).limit(experimentCount).filter(aBoolean -> aBoolean).count();
+        return count / (double) experimentCount;
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        Optional<Map.Entry<String, List<String>>> max = compositions.entrySet().stream().max((o1, o2) ->
+                        stringsCommonLength(o1.getValue().parallelStream())
+                                - stringsCommonLength(o2.getValue().parallelStream())
+        );
+        if (max.isPresent()) {
+            return max.get().getKey();
+        }
+        throw new NullPointerException("Compositions is empty!");
+    }
+
+    private static int stringsCommonLength(Stream<String> stringStream) {
+        return stringStream.mapToInt(String::length).sum();
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders.stream().flatMap(m -> m.entrySet().stream())
+                .collect(Collectors.groupingBy(
+                                Map.Entry::getKey,
+                                Collectors.summingInt(Map.Entry::getValue))
+                );
     }
 }
--- src/test/java/ru/spbau/mit/SecondPartTasksTest.java
+++ src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -1,28 +1,99 @@
 package ru.spbau.mit;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.fail;
+import java.io.UncheckedIOException;
+import java.util.*;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static ru.spbau.mit.SecondPartTasks.*;
+
 
 public class SecondPartTasksTest {
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
     @Test
     public void testFindQuotes() {
-        fail();
+        List<String> paths = Arrays.asList(
+                "src/test/resources/text1.txt",
+                "src/test/resources/text2.txt",
+                "src/test/resources/text3.txt",
+                "src/test/resources/text4.txt"
+        );
+        List<String> result = Arrays.asList(
+                "and she has no funds to reward you.",
+                "a mouth like a slash in a rubber sheet,"
+        );
+        assertEquals(result, findQuotes(paths, "she"));
+        assertEquals(Collections.emptyList(), findQuotes(paths, "never gone give you up"));
+
+        exception.expect(UncheckedIOException.class);
+        findQuotes(Collections.singletonList("wrong path"), "wrong");
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        double result = piDividedBy4();
+        assertTrue(result >= 0);
+        assertTrue(result <= 1);
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        Map<String, List<String>> writers = new HashMap<String, List<String>>() {{
+            put("Vasya", Arrays.asList("First letter", "Second Letter"));
+            put("Petya", Arrays.asList("First letter", "Second Letter"));
+            put("Sanya", Arrays.asList("First letter", "Second Letter", "Third Letter"));
+        }};
+        assertEquals("Sanya", findPrinter(writers));
+
+        exception.expect(NullPointerException.class);
+        findPrinter(Collections.emptyMap());
     }
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        Map<String, Integer> пятерочка = new HashMap<String, Integer>() {{
+            put("Водка", 300);
+            put("Молоко", 50);
+            put("Доширак", 30);
+            put("Пельмени", 150);
+        }};
+
+        Map<String, Integer> магнит = new HashMap<String, Integer>() {{
+            put("Хлеб", 20);
+            put("Картошка", 15);
+            put("Пиво", 90);
+            put("Водка", 300);
+        }};
+
+        Map<String, Integer> дикси = new HashMap<String, Integer>() {{
+            put("Чипсы", 70);
+            put("Молоко", 50);
+            put("Пиво", 90);
+            put("Доширак", 30);
+        }};
+
+        Map<String, Integer> результат = new HashMap<String, Integer>() {{
+            put("Водка", 600);
+            put("Молоко", 100);
+            put("Доширак", 60);
+            put("Пельмени", 150);
+            put("Пиво", 180);
+            put("Чипсы", 70);
+            put("Хлеб", 20);
+            put("Картошка", 15);
+        }};
+
+        assertEquals(результат,
+                calculateGlobalOrder(Arrays.asList(пятерочка, магнит, дикси))
+        );
+
+        assertEquals(Collections.emptyMap(),
+                Collections.emptyMap());
     }
 }
